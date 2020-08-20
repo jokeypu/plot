787318d5bec7921cae410dbe58663b4d9a57368e
Qing Pu <grasspq@126.com>
2020-08-20 10:51:56 +0800

    Backup: 2020年 08月 20日 星期四 10:51:56 CST

--- Shower_function.C
+++ Shower_function.C
@@ -1,9 +1,10 @@
+Double_t myfunc(Double_t x, Double_t p0, Double_t p1, Double_t p2, Double_t x0);
 int Shower_function()
 {
     int bin1(600),bin2(600);
     float tx(800),ty(600);
     double xmin(0),xmax(6),ymin(0),ymax(180);
-    TString dir_name("Gamma_1G");
+    TString dir_name("Gamma_1G_all");
     
     //******************************************//
     
@@ -35,7 +36,7 @@ int Shower_function()
     gStyle->SetTitleOffset(1.0,"xyz");
     gStyle->SetOptFit(1111);
     
-    TH1D* h1D = new TH1D("h1D","h1",50,0,5);
+    TH1D* h1D = new TH1D("h1D","h1",50,0,9);
     h1D->SetLineColor(kBlue);
     h1D->SetLineWidth(2);
     h1D->GetXaxis()->SetTitle("distance");
@@ -53,15 +54,32 @@ int Shower_function()
     f->SetParLimits(2, 0.1, 1.5);
     f->SetParLimits(3, -1.5, 1.5);
     */
-    
-    TF1 *f=new TF1("f","[0]/(pow(x-[1],2)-[2])",0,5);
+    /* 
+    TF1 *f=new TF1("f","[0]/(pow(x-[1],2)-[2])",0,6);
     f->SetLineWidth(2);
     f->SetLineColor(kRed);
-    f->SetParameters(1,0,0);
-    f->SetParLimits(0, 0, 100);
+    f->SetParameters(100,0,0);
+    f->SetParLimits(0, 50, 2000);
     f->SetParLimits(1, -3, 3);
     f->SetParLimits(2, -3, 3);
+
+    TF1 *f1=new TF1("f1","[0]*exp(-1*[1]*x)",6,15);
+    f1->SetLineWidth(2);
+    f1->SetLineColor(kRed);
+    f1->SetParameters(1,1.25);
+    f1->SetParLimits(0, 1, 200);
+    f1->SetParLimits(1, 0.01, 4);
+    */
     
+    TF1 *f=new TF1("f","myfunc(x,[0],[1],[2],[3])",0,9);
+    f->SetLineWidth(2);
+    f->SetLineColor(kRed);
+    f->SetParameters(210,-0.2,-0.1,4);
+    f->SetParLimits(0, 100, 300);
+    f->SetParLimits(1, -0.5, 0.5);
+    f->SetParLimits(2, -0.5, 0.5);
+    f->SetParLimits(3, 0, 8);
+
     int N(0);
     int num(5);
     for (Int_t ievt = 0; ievt < maxEvtNo; ievt++) {
@@ -94,8 +112,19 @@ int Shower_function()
     }
     cout << "Max Event Nomber:" << maxEvtNo << ", " << "Passed:" << N << endl;
     c1->cd();
-    h1D->Fit(f,"R");
     h1D->Draw("HIST");
+    h1D->Fit(f,"R");
     f->Draw("SAME");
     return 0;
+}
+Double_t myfunc(Double_t x, Double_t p0, Double_t p1, Double_t p2, Double_t x0) {
+    Double_t value;
+    if (x <= x0) value = p0/((x-p1) * (x-p1) - p2);
+    else{
+    Double_t fx0 = p0/((x0-p1) * (x0-p1) - p2);
+    //Double_t c = 2 * fx0 * (x0-p1) * (x0-x) / (p0*log(10));
+    Double_t c = fx0 * (x0-p1) * (x0-x) / (p0*1.1512925);
+    value = fx0 * pow(10, c); 
+    }
+    return value;
 }

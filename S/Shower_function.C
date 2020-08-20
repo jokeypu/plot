a3906e634c65a9fcb728f45ce455ccb2af6a6262
Qing Pu <grasspq@126.com>
2020-08-20 11:01:11 +0800

    Backup: 2020年 8月20日 星期四 11时01分11秒 CST

--- S/Shower_function.C
+++ S/Shower_function.C
@@ -44,6 +44,33 @@ int Shower_function()
     h1D->GetXaxis()->CenterTitle();
     h1D->GetYaxis()->CenterTitle();
     
+    /*
+    TF1 *f=new TF1("f","[0]*exp(-1*[1]*pow(x-[3],[2]))",0,5);
+    f->SetLineWidth(2);
+    f->SetLineColor(kRed);
+    f->SetParameters(300,1.25,0.8,0);
+    f->SetParLimits(0, 0, 500);
+    f->SetParLimits(1, 0.5, 8);
+    f->SetParLimits(2, 0.1, 1.5);
+    f->SetParLimits(3, -1.5, 1.5);
+    */
+    /* 
+    TF1 *f=new TF1("f","[0]/(pow(x-[1],2)-[2])",0,6);
+    f->SetLineWidth(2);
+    f->SetLineColor(kRed);
+    f->SetParameters(100,0,0);
+    f->SetParLimits(0, 50, 2000);
+    f->SetParLimits(1, -3, 3);
+    f->SetParLimits(2, -3, 3);
+
+    TF1 *f1=new TF1("f1","[0]*exp(-1*[1]*x)",6,15);
+    f1->SetLineWidth(2);
+    f1->SetLineColor(kRed);
+    f1->SetParameters(1,1.25);
+    f1->SetParLimits(0, 1, 200);
+    f1->SetParLimits(1, 0.01, 4);
+    */
+    
     TF1 *f=new TF1("f","myfunc(x,[0],[1],[2],[3])",0,9);
     f->SetLineWidth(2);
     f->SetLineColor(kRed);
@@ -95,6 +122,7 @@ Double_t myfunc(Double_t x, Double_t p0, Double_t p1, Double_t p2, Double_t x0) 
     if (x <= x0) value = p0/((x-p1) * (x-p1) - p2);
     else{
     Double_t fx0 = p0/((x0-p1) * (x0-p1) - p2);
+    //Double_t c = 2 * fx0 * (x0-p1) * (x0-x) / (p0*log(10));
     Double_t c = fx0 * (x0-p1) * (x0-x) / (p0*1.1512925);
     value = fx0 * pow(10, c); 
     }

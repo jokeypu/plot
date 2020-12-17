2a909b9d1346080235c0ae2c5da82da5cbf9ee14
Qing Pu <grasspq@126.com>
2020-12-18 02:59:45 +0800

    Backup: 2020年12月18日 星期五 02时59分45秒 CST

---
+++ Shower_instance_plus.C
@@ -0,0 +1,80 @@
+int Shower_instance_plus(string old_file, string new_file)
+{
+    int bin1(100),bin2(200);
+    float tx(1200),ty(900);
+    double xmin(0.7),xmax(1.3);
+    cout << "-INFO  Old File : " << old_file << endl;
+    cout << "-INFO  New File : " << new_file << endl;
+    
+    
+    string file_name1 = "doc/" + old_file;
+    string file_name2 = "doc/" + new_file;
+    
+    TCanvas* c1=new TCanvas("PANDA1","c1",tx,ty);
+    gStyle->SetOptTitle(0);
+    gStyle->SetStatX(0.36);
+    gStyle->SetStatY(0.88);
+    gStyle->SetOptStat(1);
+    gStyle->SetLabelFont(42,"xyz");
+    gStyle->SetLabelSize(0.05,"xyz");
+    gStyle->SetLabelOffset(0.01,"xyz");
+    gStyle->SetNdivisions(510,"xyz");
+    gStyle->SetTitleFont(42,"xyz");
+    gStyle->SetTitleColor(1,"xyz");
+    gStyle->SetTitleSize(0.05,"xyz");
+    gStyle->SetTitleOffset(1.0,"xyz");
+    
+    TH1D* h1D1 = new TH1D("Hist1_1","h1_1", bin1, xmin, xmax);
+    h1D1->SetLineColor(kGray+3);
+    h1D1->SetLineWidth(2);
+    h1D1->GetXaxis()->SetTitle("Energy");
+    h1D1->GetYaxis()->SetTitle("Entries");
+    h1D1->GetXaxis()->CenterTitle();
+    h1D1->GetYaxis()->CenterTitle();
+    
+    TH1D* h1D2 = new TH1D("Hist1_2","h1_2", bin1, xmin, xmax);
+    h1D2->SetLineColor(kRed);
+    h1D2->SetLineWidth(2);
+    h1D2->GetXaxis()->SetTitle("Energy");
+    h1D2->GetYaxis()->SetTitle("Entries");
+    h1D2->GetXaxis()->CenterTitle();
+    h1D2->GetYaxis()->CenterTitle();
+
+    string str1, str2;
+
+    ifstream file1;
+    file1.open(file_name1, ios::in);
+    Int_t num1(0);
+    while (getline(file1,str1)) num1++;
+    cout << num1 << endl;
+    file1.seekg(0, ios::beg);
+    
+    ifstream file2;
+    file2.open(file_name2, ios::in);
+    Int_t num2(0);
+    while (getline(file2,str2)) num2++;
+    cout << num2 << endl;
+    file1.seekg(0, ios::beg);
+    
+    Int_t MaxNo = num1 < num2 ? num1 : num2;
+    for (int i = 0; i < MaxNo; i++) {
+        if (!getline(file1,str1)) return 1;
+        if (!getline(file2,str2)) return 1;
+        double value1= atof(str1.c_str());
+        double value2= atof(str2.c_str());
+        h1D1->Fill(value1);
+        h1D2->Fill(value2);
+    }
+    
+    c1->cd();
+    h1D2->Draw();
+    h1D1->Draw("SAME");
+   
+    TLegend * leg = new TLegend(0.7,0.7 , 0.9, 0.8);
+    leg->AddEntry(h1D1, old_file, "L");
+    leg->AddEntry(h1D2, new_file, "L");
+    //leg->AddEntry(h1D1,"Bump Energy old" , "L");
+    //leg->AddEntry(h1D2,"Bump Energy new", "L");
+    leg->Draw();
+    return 0;
+}

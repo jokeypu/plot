8b0439069899dff141f409d1c9bc556edba3c2ff
Qing Pu <grasspq@126.com>
2020-12-27 09:51:45 +0800

    Backup: 2020年12月27日 星期日 09时51分45秒 CST

--- Fit_All_Angle_Par.C
+++ Fit_All_Angle_Par.C
@@ -134,21 +134,53 @@ int Fit_All_Angle_Par(){
     }
     par_file.close();
 
-    TF1* f11=new TF1("f11","[0]*exp(-[1]*x)+[2]",30,130);
+    TF1* f11=new TF1("f11","[0]",30,130);
     f11->SetLineColor(kRed-7);
-    f11->SetParameters(-0.16,0.24,0.28);
     
-    TF1* f12=new TF1("f12","[0]*exp(-[1]*x)+[2]",30,130);
+    TF1* f12=new TF1("f12","[0]",30,130);
     f12->SetLineColor(kRed-7);
-    f12->SetParameters(0.3,0.6,1.4);
     
-    TF1* f13=new TF1("f13","[0]*exp(-[1]*x)+[2]",30,130);
+    TF1* f13=new TF1("f13","[0]*(x-[1])*(x-[1])+[2]",30,130);
     f13->SetLineColor(kRed-7);
-    f13->SetParameters(0.04,1.5,0.004);
+    f13->SetParameters(0.04,80,0.004);
     
-    //g11->Fit(f11,"R");
-    //g12->Fit(f12,"R");
-    //g13->Fit(f13,"R");
+    TF1* f21=new TF1("f21","[0]",30,130);
+    f21->SetLineColor(kRed-7);
+    
+    TF1* f22=new TF1("f22","[0]",30,130);
+    f22->SetLineColor(kRed-7);
+    
+    TF1* f23=new TF1("f23","[0]*(x-[1])*(x-[1])+[2]",30,130);
+    f23->SetLineColor(kRed-7);
+    f23->SetParameters(0.04,80,0.004);
+    
+    TF1* f31=new TF1("f31","[0]",30,130);
+    f31->SetLineColor(kRed-7);
+    
+    TF1* f32=new TF1("f32","[0]",30,130);
+    f32->SetLineColor(kRed-7);
+    
+    TF1* f33=new TF1("f33","[0]*(x-[1])*(x-[1])+[2]",30,130);
+    f33->SetLineColor(kRed-7);
+    f33->SetParameters(0.04,80,0.004);
+    
+    TF1* f41=new TF1("f41","[0]",30,130);
+    f41->SetLineColor(kRed-7);
+    
+    TF1* f42=new TF1("f42","[0]",30,130);
+    f42->SetLineColor(kRed-7);
+    
+    g11->Fit(f11,"R");
+    g12->Fit(f12,"R");
+    g13->Fit(f13,"R");
+    g21->Fit(f21,"R");
+    g22->Fit(f22,"R");
+    g23->Fit(f23,"R");
+    g31->Fit(f31,"R");
+    g32->Fit(f32,"R");
+    g33->Fit(f33,"R");
+    g41->Fit(f41,"R");
+    g42->Fit(f42,"R");
     
     c1->Divide(1, 3);
     c1->cd(1);

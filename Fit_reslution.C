db6ec16a2ff74984e5809becafb21749fa7447f9
Qing Pu <grasspq@126.com>
2020-12-28 17:44:05 +0800

    Backup: 2020年12月28日 星期一 17时44分05秒 CST

---
+++ Fit_reslution.C
@@ -0,0 +1,57 @@
+int Fit_resolution(Int_t NO_Angle){
+    //title[20] = "doc/"+ dir_name +"_R.txt"
+    ostringstream out1;
+    out1 << NO_Angle;
+    string str_NO_Angle = out1.str();
+    std::string out_name = "doc/A"+str_NO_Angle+"_resolution_par.txt";
+    std::ifstream par_file;
+    par_file.open(out_name,std::ios::in);
+    
+    TString ts = "Angle "+str_NO_Angle+"0 deg";
+    TCanvas* c1=new TCanvas("PANDA1",ts,1000,700);
+    gStyle->SetOptTitle(0);
+    gStyle->SetStatX(0.36);
+    gStyle->SetStatY(0.88);
+    gStyle->SetOptStat(0);
+    gStyle->SetLabelFont(42,"xyz");
+    gStyle->SetLabelSize(0.05,"xyz");
+    gStyle->SetLabelOffset(0.01,"xyz");
+    gStyle->SetNdivisions(510,"xyz");
+    gStyle->SetTitleFont(42,"xyz");
+    gStyle->SetTitleColor(1,"xyz");
+    gStyle->SetTitleSize(0.05,"xyz");
+    gStyle->SetTitleOffset(1.0,"xyz");
+    //gStyle->SetOptFit(1111);
+    
+    TGraph *g1 = new TGraph();
+    g1->SetMarkerStyle(20);
+    g1->SetMarkerColorAlpha(kRed-3, 1);
+    g1->GetXaxis()->SetTitle("Energy");
+    g1->GetYaxis()->SetTitle("Resolution");
+    g1->GetXaxis()->CenterTitle();
+    g1->GetYaxis()->CenterTitle();
+    
+    TGraph *g2 = new TGraph();
+    g2->SetMarkerStyle(22);
+    g2->SetMarkerColorAlpha(kGreen+1, 1);
+    g2->GetXaxis()->SetTitle("Energy");
+    g2->GetYaxis()->SetTitle("Resolution");
+    g2->GetXaxis()->CenterTitle();
+    g2->GetYaxis()->CenterTitle();
+    
+    string str;
+    Int_t N = 0;
+    while (std::getline(par_file, str)) {
+        std::stringstream strStream(str);
+        float energy, R1, ER1, R2, ER2;
+        strStream >> energy >> R1 >> ER1 >> R2 >> ER2;
+        g1->SetPoint(N,energy,R1);
+        g2->SetPoint(N,energy,R2);
+        N++;
+    }
+    
+    g1->Draw("AP.");
+    g2->Draw("AP.same");
+    
+    return 0;
+}

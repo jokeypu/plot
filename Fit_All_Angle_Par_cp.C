b32191cc1206998d137be495ec34ec1318f07d51
Qing Pu <grasspq@126.com>
2020-12-29 19:10:54 +0800

    Backup: 2020年12月29日 星期二 19时10分54秒 CST

--- Fit_All_Angle_Par_cp.C
+++ Fit_All_Angle_Par_cp.C
@@ -168,7 +168,7 @@ int Fit_All_Angle_Par_cp(){
     f22->SetLineColor(kRed-7);
     
     //TF1* f23=new TF1("f23","[0]*(x-[1])*(x-[1])+[2]",30,130);
-    TF1* f23=new TF1("f23","[0]*(x-[1])*(x-[1])+[2]+6.3*TMath::Gaus(x,80,41)",30,130);
+    TF1* f23=new TF1("f23","[0]*(x-[1])*(x-[1])+[2]+0.015*TMath::Gaus(x,81,5)",30,130);
     f23->SetLineColor(kRed-7);
     f23->SetParameters(0.04,90,0.004);
     

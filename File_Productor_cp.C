41738a15d044ef294cc6250a19b01ae28d04328a
Qing Pu <grasspq@126.com>
2021-01-18 08:03:00 +0800

    Backup: 2021年 1月18日 星期一 08时03分00秒 CST

---
+++ File_Productor_cp.C
@@ -0,0 +1,195 @@
+Double_t DD(const TVector3 *DetPos, const TVector3 *Cent, const Double_t par){
+    Double_t distance(0), angle(0);
+    if (*DetPos != *Cent) {
+        TVector3 vz(0, 0, 1);
+        TVector3 DetPos_o = (*DetPos) - 3.7*vz;
+        TVector3 DetPos_n;
+        DetPos_n.SetMagThetaPhi(DetPos_o.Mag(), DetPos_o.Theta(), DetPos_o.Phi()-0.06981317);
+        TVector3 ey = DetPos_n.Cross(vz).Unit();
+        TVector3 ex = DetPos_n.Cross(ey).Unit();
+        Double_t dx = abs((*Cent-*DetPos).Dot(ex));
+        Double_t dy = abs((*Cent-*DetPos).Dot(ey));
+        distance = sqrt(dx*dx+dy*dy);
+        angle = 57.2957*TMath::ATan(dy/dx);
+        if ( angle > 90 && angle <= 180 ) angle = 180 - angle;
+        if ( angle > 45 && angle <= 90 ) angle = 90 - angle;
+    }
+    return distance;
+}
+
+Double_t AA(const TVector3 *DetPos, const TVector3 *Cent, const Double_t par){
+    Double_t distance(0), angle(0);
+    if (*DetPos != *Cent) {
+        TVector3 vz(0, 0, 1);
+        TVector3 DetPos_o = (*DetPos) - 3.7*vz;
+        TVector3 DetPos_n;
+        DetPos_n.SetMagThetaPhi(DetPos_o.Mag(), DetPos_o.Theta(), DetPos_o.Phi()-0.06981317);
+        TVector3 ey = DetPos_n.Cross(vz).Unit();
+        TVector3 ex = DetPos_n.Cross(ey).Unit();
+        Double_t dx = (*DetPos-*Cent).Dot(ex);
+        Double_t dy = (*DetPos-*Cent).Dot(ey);
+        TVector2 vv(dx,dy);
+        distance = sqrt(dx*dx+dy*dy);
+        angle = fabs(TMath::RadToDeg()*vv.Phi_mpi_pi(vv.Phi()));
+        //if ( angle > 90 && angle <= 180 ) angle = 180 - angle;
+        //if ( angle > 45 && angle <= 90 ) angle = 90 - angle;
+    }
+    return angle;
+}
+
+int File_Productor_cp(std::string dir_name){
+    //std::string dir_name="Gamma_one_1G";
+    std::string out_name = "doc/"+ dir_name +".txt";
+    
+    std::ofstream File_out;
+    File_out.open(out_name,std::ios::out);
+    //File_out << "Index " << "Distance " << "Angle " << "Energy" << endl;
+    
+    Int_t NGamma(1);
+    
+    TString file_path_sim = "../data/"+dir_name+"/evtcomplete_sim.root";
+    TString file_path_digi = "../data/"+dir_name+"/evtcomplete_digi.root";
+    
+    FairRunAna *fRun = new FairRunAna();
+    TFile* file = new TFile(file_path_sim);
+    FairFileSource* source = new FairFileSource(file,"InputFile");
+    FairRootManager* ioman = FairRootManager::Instance();
+    ioman->SetSource(source);
+    ioman->InitSource();
+    
+    TClonesArray* fMCTrackArray = (TClonesArray*) ioman->GetObject("MCTrack");
+    if (!fMCTrackArray) return -1;
+    TClonesArray* fHitArray = (TClonesArray*) ioman->GetObject("EmcHit");
+    if (!fHitArray) return -1;
+    
+    TFile* f = new TFile(file_path_digi);
+    TTree* t = (TTree*)f->Get("pndsim");
+    TClonesArray* fBumpArray = new TClonesArray("PndEmcBump");
+    t->SetBranchAddress("EmcBump",&fBumpArray);
+    TClonesArray* fDigiArray = new TClonesArray("PndEmcDigi");
+    t->SetBranchAddress("EmcDigi",&fDigiArray);
+    if (!fBumpArray) return -1;
+    TClonesArray* fClusterArray = new TClonesArray("PndEmcCluster");
+    t->SetBranchAddress("EmcCluster",&fClusterArray);
+    if (!fClusterArray) return -1;
+    
+    int N(0);
+    int SEED_CUT(-1);
+    Int_t maxEvtNo = t->GetEntries();
+    //maxEvtNo /= 10;
+    for (Int_t ievt = 0; ievt < maxEvtNo; ievt++) {
+        ioman->ReadEvent(ievt); // read event by event
+        t->GetEntry(ievt);
+        if (ievt%(maxEvtNo/100)==0) cout << 100 * (int)ievt/maxEvtNo << "%" << endl;
+        int nhits = fHitArray->GetEntriesFast();
+        int nclusters = fClusterArray->GetEntriesFast();
+        int nbumps = fBumpArray->GetEntriesFast();
+        int ndigis = fDigiArray->GetEntriesFast();
+        
+        //Get the momentum of each photon
+        std::vector<TVector3> Gamma_mom;
+        for (int iGamma = 0; iGamma < NGamma; iGamma++) {
+            PndMCTrack *mcTrack = (PndMCTrack *)fMCTrackArray->At(iGamma);
+            Gamma_mom.push_back(mcTrack->GetMomentum());
+        }
+        
+        //Calculate the average distance between photons
+        Double_t distance(0);
+        Int_t Ncunt(0);
+        for (int iGamma = 0; iGamma < NGamma-1; iGamma++) {
+            for (int jGamma = iGamma+1; jGamma < NGamma; jGamma++) {
+                Double_t TheDistance = ((65.0/Gamma_mom[iGamma].Pt())*Gamma_mom[iGamma]-(65.0/Gamma_mom[jGamma].Pt())*Gamma_mom[jGamma]).Mag();
+                distance += TheDistance;
+                Ncunt++;
+            }
+        }
+        distance /= Ncunt;
+        
+        //Exclude events generated electron-positron
+        std::map<Int_t, bool> Exist;
+        for (int i = 0; i < nhits; i++) {
+            PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
+            std::set<FairLink> links = (hit->GetTrackEntering()).GetLinks();
+            for (std::set<FairLink>::iterator linkIter = links.begin(); linkIter != links.end(); linkIter++) {
+                for (int iGamma = 0; iGamma < NGamma; iGamma++)
+                    if (linkIter->GetIndex() == iGamma) Exist[iGamma] = true;
+            }
+        }
+        if (Exist.size() != NGamma) continue;
+        
+        //Get the true energy of each shower
+        std::map<Int_t, Double_t> truth_E;
+        for (int iGamma = 0; iGamma < NGamma; iGamma++) truth_E[iGamma] = 0.0;
+        for (int i = 0; i < nhits; i++) {
+            PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
+            std::map<Int_t, Double_t>  dep = hit->GetDepositedEnergyMap();
+            std::map<Int_t, Double_t>::iterator ptr;
+            for ( ptr = dep.begin(); ptr != dep.end(); ptr++) {
+                for (int iGamma = 0; iGamma < NGamma; iGamma++)
+                    if (ptr->first == iGamma) truth_E[iGamma] += ptr->second;
+            }
+        }
+        
+        //Match bump for each photon
+        std::vector<Int_t> match;
+        for (int iGamma = 0; iGamma < NGamma; iGamma++) {
+            Double_t min_d(99999);
+            Int_t index(-1);
+            for (int i = 0; i < nbumps; i++) {
+                PndEmcBump* Bump = (PndEmcBump*)fBumpArray->At(i);
+                TVector3 pos = Bump->position();
+                Double_t d = pos.Mag()*sin(Gamma_mom[iGamma].Angle(pos));
+                if (d < min_d) { min_d = d; index = i; }
+            }
+            if ( index == -1 ) return 1;
+            match.push_back(index);
+        }
+        
+        PndEmcBump* Bump = (PndEmcBump*)fBumpArray->At(0);
+        PndEmcCluster *theCluster = (PndEmcCluster *)fClusterArray->At(0);
+        std::map<Int_t, Int_t> theMaximaDigis = theCluster->LocalMaxMap();
+        
+        std::map<Int_t, Int_t>::iterator p;
+        Int_t digi_seed(-1),digi_seed_id(-1);
+        int c(0);
+        for (p = theMaximaDigis.begin(); p != theMaximaDigis.end(); p++){
+            digi_seed = p->second;
+            digi_seed_id = p->first;
+            c++;
+        }
+        if ( c!=1 ) continue;
+        if (ievt==0) SEED_CUT = digi_seed_id;
+        if (SEED_CUT != digi_seed_id) continue;
+        
+        PndEmcDigi* digi = (PndEmcDigi*)fDigiArray->At(digi_seed);
+        double Seed_Energy = digi->GetEnergy();
+        TVector3 Seed_pos = digi->where();
+        TVector3 Cent_pos = Bump->where();
+        
+        for (int i = 0; i < nhits; i++) {
+            PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
+            //if (hit->GetDetectorID() == digi_seed_id) continue;
+            TVector3 Det_Pos;
+            double Truth_Energy = hit->GetEnergy();
+            double Digi_Energy = -1;
+            for (int j = 0 ; j < ndigis ; j++){
+                PndEmcDigi* idigi = (PndEmcDigi*)fDigiArray->At(j);
+                if (idigi->GetDetectorId() == hit->GetDetectorID()) {
+                    Digi_Energy = idigi->GetEnergy();
+                    Det_Pos = idigi->where();
+                }
+            }
+            if (Digi_Energy == -1) continue;
+            double Distance = DD(&Det_Pos, &Cent_pos, 1.25);
+            double angle = AA(&Det_Pos, &Cent_pos, 1.25);
+            if ( angle > 90 && angle <= 180 ) angle = 180 - angle;
+            if ( angle > 45 && angle <= 90 ) angle = 90 - angle;
+            if (Distance > 14) continue;
+            N++;
+            File_out << Distance << " " << angle << " " << Truth_Energy << endl;
+        }
+    }
+    File_out.close();
+    cout << "Max Event Nomber:" << maxEvtNo << ", " << "Passed:" << N << endl;
+    return 0;
+}

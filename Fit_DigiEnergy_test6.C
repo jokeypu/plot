1e7887fb2bd29ea066d32653b2e21cb755da9f36
Qing Pu <grasspq@126.com>
2021-01-22 16:11:34 +0800

    Backup: 2021年 1月22日 星期五 16时11分34秒 CST

--- Fit_DigiEnergy_test6.C
+++ Fit_DigiEnergy_test6.C
@@ -34,8 +34,8 @@ int Fit_DigiEnergy_test6( Int_t NO_Angle = 7, Double_t Energy = 1.0){
     gStyle->SetTitleOffset(1.0,"xyz");
     
     //std::string file_name(title);
-    std::string file_name = "doc/WorkData_1Gamma_A4_E1.5_OR.txt";
-    Energy = 1.5;
+    std::string file_name = "doc/WorkData_1Gamma_A7_E0.8_OR.txt";
+    Energy = 0.8;
     std::ifstream in_file;
     in_file.open(file_name,std::ios::in);
     

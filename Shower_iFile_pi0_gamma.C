1214e518d20a050e67200924a848d4ea30f06e5e
Qing Pu <grasspq@126.com>
2021-02-02 13:36:52 +0800

    Backup: 2021年 2月 2日 星期二 13时36分52秒 CST

--- Shower_iFile_pi0_gamma.C
+++ Shower_iFile_pi0_gamma.C
@@ -3,7 +3,7 @@ int Shower_iFile_pi0_gamma(string dir_name)
 {
     string out_name1 = "doc/" + dir_name + "_pi0_gamma_min.txt";
     string out_name2 = "doc/" + dir_name + "_pi0_gamma_max.txt";
-    if( Exec( dir_name, out_name1, out_name2, 2, true) ) return 1;
+    if( Exec( dir_name, out_name1, out_name2, 3, true) ) return 1;
     return 0;
 }
 
@@ -55,7 +55,7 @@ int Exec(string dir_name, string out_name_min, string out_name_max, Int_t NGamma
         
         //Get the momentum of each photon
         std::vector<TVector3> Gamma_mom;
-        for (int iGamma = 0; iGamma < NGamma; iGamma++) {
+        for (int iGamma = 1; iGamma < NGamma; iGamma++) {
             PndMCTrack *mcTrack = (PndMCTrack *)fMCTrackArray->At(iGamma);
             Gamma_mom.push_back(mcTrack->GetMomentum());
         }
@@ -66,7 +66,7 @@ int Exec(string dir_name, string out_name_min, string out_name_max, Int_t NGamma
             PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
             std::set<FairLink> links = (hit->GetTrackEntering()).GetLinks();
             for (std::set<FairLink>::iterator linkIter = links.begin(); linkIter != links.end(); linkIter++) {
-                for (int iGamma = 0; iGamma < NGamma; iGamma++)
+                for (int iGamma = 1; iGamma < NGamma; iGamma++)
                     if (linkIter->GetIndex() == iGamma) Exist[iGamma] = true;
             }
         }
@@ -75,7 +75,7 @@ int Exec(string dir_name, string out_name_min, string out_name_max, Int_t NGamma
         //Calculate the average distance between photons
         Double_t distance(0);
         Int_t Ncunt(0);
-        for (int iGamma = 0; iGamma < NGamma-1; iGamma++) {
+        for (int iGamma = 1; iGamma < NGamma-1; iGamma++) {
             for (int jGamma = iGamma+1; jGamma < NGamma; jGamma++) {
                 Double_t TheDistance = ((65.0/Gamma_mom[iGamma].Pt())*Gamma_mom[iGamma]-(65.0/Gamma_mom[jGamma].Pt())*Gamma_mom[jGamma]).Mag();
                 //Double_t TheDistance = 2 * 65.0 * sin(Gamma_mom[iGamma].Angle(Gamma_mom[jGamma])/2.0);
@@ -87,20 +87,20 @@ int Exec(string dir_name, string out_name_min, string out_name_max, Int_t NGamma
 
         //Get the true energy of each shower
         std::map<Int_t, Double_t> truth_E;
-        for (int iGamma = 0; iGamma < NGamma; iGamma++) truth_E[iGamma] = 0.0;
+        for (int iGamma = 1; iGamma < NGamma; iGamma++) truth_E[iGamma] = 0.0;
         for (int i = 0; i < nhits; i++) {
             PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
             std::map<Int_t, Double_t>  dep = hit->GetDepositedEnergyMap();
             std::map<Int_t, Double_t>::iterator ptr;
             for ( ptr = dep.begin(); ptr != dep.end(); ptr++) {
-                for (int iGamma = 0; iGamma < NGamma; iGamma++)
+                for (int iGamma = 1; iGamma < NGamma; iGamma++)
                     if (ptr->first == iGamma) truth_E[iGamma] += ptr->second;
             }
         }
         
         //Match bump for each photon
         std::vector<Int_t> match;
-        for (int iGamma = 0; iGamma < NGamma; iGamma++) {
+        for (int iGamma = 1; iGamma < NGamma; iGamma++) {
             Double_t min_d(99999);
             Int_t index(-1);
             for (int i = 0; i < nbumps; i++) {
@@ -127,12 +127,12 @@ int Exec(string dir_name, string out_name_min, string out_name_max, Int_t NGamma
         //Calculate the error of energy and position
         Double_t bump_E_min(-1.0), bump_E_max(-1.0);
         Double_t bump_E[2];
-        for (int iGamma = 0; iGamma < NGamma; iGamma++) {
+        for (int iGamma = 1; iGamma < NGamma; iGamma++) {
             PndEmcBump* Bump = (PndEmcBump*)fBumpArray->At(match[iGamma]);
             bump_E[iGamma] = Bump->energy();
         }
-        cout << bump_E[0] << " " << truth_E[0] << endl;
-        cout << bump_E[1] << " " << truth_E[1] << endl;
+        //cout << bump_E[0] << " " << truth_E[0] << endl;
+        //cout << bump_E[1] << " " << truth_E[1] << endl;
         if (bump_E[0] < bump_E[1]) {
             out_min << bump_E[0] << " " << truth_E[0] << endl;
             out_max << bump_E[1] << " " << truth_E[1] << endl;

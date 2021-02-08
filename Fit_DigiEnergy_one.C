1df0b88ebc01731af6a2048ad4593f32fb4deae2
Qing Pu <grasspq@126.com>
2021-02-08 18:56:54 +0800

    Backup: 2021年 2月 8日 星期一 18时56分54秒 CST

--- Fit_DigiEnergy_one.C
+++ Fit_DigiEnergy_one.C
@@ -30,15 +30,15 @@ int Fit_DigiEnergy_one(std::string dir_name, const char title[30], Int_t NO_Angl
     gStyle->SetTitleOffset(1.2,"xyz");
     //gStyle->SetPalette(1);
     
-    c1->SetLeftMargin(0.15);
-    c1->SetRightMargin(0.15);
-    c1->SetTopMargin(-0.15);
-    c1->SetBottomMargin(0.15);
+    c1->SetLeftMargin(0.13);
+    c1->SetRightMargin(0.13);
+    c1->SetTopMargin(-0.13);
+    c1->SetBottomMargin(0.13);
     
-    c2->SetLeftMargin(0.15);
-    c2->SetRightMargin(0.15);
-    c2->SetTopMargin(-0.15);
-    c2->SetBottomMargin(0.15);
+    c2->SetLeftMargin(0.13);
+    c2->SetRightMargin(0.13);
+    c2->SetTopMargin(-0.13);
+    c2->SetBottomMargin(0.13);
     
     std::string file_name(title);
     std::ifstream in_file;

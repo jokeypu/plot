56a9240a499cbade4114178b06c418e5381cf39c
Qing Pu <grasspq@126.com>
2021-02-08 17:04:43 +0800

    Backup: 2021年 2月 8日 星期一 17时04分43秒 CST

---
+++ File_Productor_compare.C
@@ -0,0 +1,158 @@
+Double_t DD(const TVector3 *DetPos, const TVector3 *Cent, const Double_t par){
+    Double_t distance(0), angle(0);
+    if (*DetPos != *Cent) {
+        TVector3 vz(0, 0, 1);
+        TVector3 DetPos_o = (*DetPos) - 3.7*vz;
+        TVector3 DetPos_n;
+        DetPos_n.SetMagThetaPhi(DetPos_o.Mag(), DetPos_o.Theta(), DetPos_o.Phi()-0.06981317);
+        TVector3 ey = DetPos_n.Cross(vz).Unit();
+        TVector3 ex = DetPos_n.Cross(ey).Unit();
+        Double_t dx = abs((*Cent-*DetPos).Dot(ex));
+        Double_t dy = abs((*Cent-*DetPos).Dot(ey));
+        distance = sqrt(dx*dx+dy*dy);
+        angle = 57.2957*TMath::ATan(dy/dx);
+        if ( angle > 90 && angle <= 180 ) angle = 180 - angle;
+        if ( angle > 45 && angle <= 90 ) angle = 90 - angle;
+    }
+    return distance;
+}
+
+Double_t AA(const TVector3 *DetPos, const TVector3 *Cent, const Double_t par){
+    Double_t distance(0), angle(0);
+    if (*DetPos != *Cent) {
+        TVector3 vz(0, 0, 1);
+        TVector3 DetPos_o = (*DetPos) - 3.7*vz;
+        TVector3 DetPos_n;
+        DetPos_n.SetMagThetaPhi(DetPos_o.Mag(), DetPos_o.Theta(), DetPos_o.Phi()-0.06981317);
+        TVector3 ey = DetPos_n.Cross(vz).Unit();
+        TVector3 ex = DetPos_n.Cross(ey).Unit();
+        Double_t dx = (*DetPos-*Cent).Dot(ex);
+        Double_t dy = (*DetPos-*Cent).Dot(ey);
+        TVector2 vv(dx,dy);
+        distance = sqrt(dx*dx+dy*dy);
+        angle = fabs(TMath::RadToDeg()*vv.Phi_mpi_pi(vv.Phi()));
+        //if ( angle > 90 && angle <= 180 ) angle = 180 - angle;
+        //if ( angle > 45 && angle <= 90 ) angle = 90 - angle;
+    }
+    return angle;
+}
+
+int File_Productor_compare(std::string dir_name, Double_t Shower_Energy){
+    //std::string dir_name="Gamma_one_1G";
+    std::string out_name = "doc/compare_"+ dir_name +".txt";
+    
+    std::ofstream File_out;
+    File_out.open(out_name,std::ios::out);
+    //File_out << "Index " << "Distance " << "Angle " << "Energy" << endl;
+    
+    Int_t NGamma(1);
+    
+    TString file_path_sim = "../data/"+dir_name+"/evtcomplete_sim.root";
+    TString file_path_digi = "../data/"+dir_name+"/evtcomplete_digi.root";
+    
+    FairRunAna *fRun = new FairRunAna();
+    TFile* file = new TFile(file_path_sim);
+    FairFileSource* source = new FairFileSource(file,"InputFile");
+    FairRootManager* ioman = FairRootManager::Instance();
+    ioman->SetSource(source);
+    ioman->InitSource();
+    
+    TClonesArray* fMCTrackArray = (TClonesArray*) ioman->GetObject("MCTrack");
+    if (!fMCTrackArray) return -1;
+    TClonesArray* fHitArray = (TClonesArray*) ioman->GetObject("EmcHit");
+    if (!fHitArray) return -1;
+    
+    TFile* f = new TFile(file_path_digi);
+    TTree* t = (TTree*)f->Get("pndsim");
+    TClonesArray* fBumpArray = new TClonesArray("PndEmcBump");
+    t->SetBranchAddress("EmcBump",&fBumpArray);
+    TClonesArray* fDigiArray = new TClonesArray("PndEmcDigi");
+    t->SetBranchAddress("EmcDigi",&fDigiArray);
+    if (!fBumpArray) return -1;
+    TClonesArray* fClusterArray = new TClonesArray("PndEmcCluster");
+    t->SetBranchAddress("EmcCluster",&fClusterArray);
+    if (!fClusterArray) return -1;
+    
+    int N(0),CC(0);
+    int SEED_CUT(-1);
+    Int_t maxEvtNo = ioman->CheckMaxEventNo();
+    //maxEvtNo /= 10;
+    for (Int_t ievt = 0; ievt < maxEvtNo; ievt++) {
+        ioman->ReadEvent(ievt); // read event by event
+        t->GetEntry(ievt);
+        if (maxEvtNo>=100 && ievt%(maxEvtNo/100)==0) cout << 100 * (int)ievt/maxEvtNo << "%" << endl;
+        int nhits = fHitArray->GetEntriesFast();
+        int nclusters = fClusterArray->GetEntriesFast();
+        int nbumps = fBumpArray->GetEntriesFast();
+        int ndigis = fDigiArray->GetEntriesFast();
+        
+        //if (nbumps==0) continue;
+        PndEmcBump* Bump;
+        Double_t Bump_E=-1;
+        for (int i = 0; i < nbumps; i++){
+             PndEmcBump* Bump_i = (PndEmcBump*)fBumpArray->At(i);
+            if ((Bump_i->energy())>Bump_E) Bump = Bump_i;
+        }
+        
+        bool Exist = false;
+        for (int i = 0; i < nhits; i++) {
+            PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
+            std::set<FairLink> links = (hit->GetTrackEntering()).GetLinks();
+            for (std::set<FairLink>::iterator linkIter = links.begin(); linkIter != links.end(); linkIter++) {
+                    if (linkIter->GetIndex() == 0) Exist = true;
+            }
+        }
+        if (!Exist) continue;
+        
+        TVector3 Cent_pos = Bump->where();
+        
+        double E_max = -1.0;
+        double min_distance = 99999;
+        for (int i = 0; i < nhits; i++) {
+            PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
+            TVector3 Det_Pos;
+            hit->Position(Det_Pos);
+            double Distance = DD(&Det_Pos, &Cent_pos, 1.25);
+            if (Distance < min_distance) {min_distance = Distance;E_max = hit->GetEnergy();}
+        }
+        //cout << E_max << endl;
+        if (E_max < Shower_Energy/4.0) continue;
+        
+        PndEmcCluster *theCluster = (PndEmcCluster *)fClusterArray->At(0);
+        std::map<Int_t, Int_t> theMaximaDigis = theCluster->LocalMaxMap();
+        
+        std::map<Int_t, Int_t>::iterator p;
+        Int_t digi_seed(-1),digi_seed_id(-1);
+        int c(0);
+        for (p = theMaximaDigis.begin(); p != theMaximaDigis.end(); p++){
+            digi_seed = p->second;
+            digi_seed_id = p->first;
+            c++;
+        }
+        if ( c!=1 ) continue;
+        
+        PndEmcDigi* digi = (PndEmcDigi*)fDigiArray->At(digi_seed);
+        double Seed_Energy = digi->GetEnergy();
+        TVector3 Seed_pos = digi->where();
+        
+        for (int i = 0; i < nhits; i++) {
+            PndEmcHit* hit = (PndEmcHit*)fHitArray->At(i);
+            TVector3 Det_Pos;
+            hit->Position(Det_Pos);
+            double Truth_Energy = hit->GetEnergy();
+            double Distance = DD(&Det_Pos, &Cent_pos, 1.25);
+            double dseed = DD(&Det_Pos, &Cent_pos, 1.25);
+            double angle = AA(&Seed_pos, &Cent_pos, 1.25);
+            if ( angle > 90 && angle <= 180 ) angle = 180 - angle;
+            if ( angle > 45 && angle <= 90 ) angle = 90 - angle;
+            if (Distance > 4) continue;
+            //if (Distance <1.5 && Truth_Energy < Shower_Energy/5.0) continue;
+            File_out << dseed << " " << Distance << " " << Seed_Energy << " " << Truth_Energy << endl;
+            N++;
+        }
+        CC++;
+    }
+    File_out.close();
+    cout << "Max Event Nomber:" << maxEvtNo << ", " << "Passed:" << N << "   total:" << CC << endl;
+    return 0;
+}
---
+++ Fit_DigiEnergy_compare.C
@@ -0,0 +1,148 @@
+const Double_t RM = 2.00;
+Double_t FABC(Double_t r,Double_t p0, Double_t p1, Double_t p2, Double_t p3, Double_t p4){
+    Double_t xi = r - p2*r*exp(-pow(r/p3/RM,p4));
+    return p0*exp(-p1*xi/RM);
+}
+
+int Fit_DigiEnergy_compare(std::string dir_name, const char title[30], Int_t NO_Angle, Double_t Energy){
+    //title[20] = "doc/"+ dir_name +"_R.txt"
+    ostringstream out1,out2;
+    out1 << NO_Angle;
+    out2 << fixed << setprecision(1) << Energy;
+    string str_NO_Angle = out1.str(), str_Energy = out2.str();
+    //std::string out_name = "doc/compare_A"+str_NO_Angle+"_par_cp.txt";
+    //std::ofstream par_file;
+    //par_file.open(out_name,std::ios::app);
+    
+    TCanvas* c1=new TCanvas("PANDA1","test1",800,600);
+    TCanvas* c2=new TCanvas("PANDA2","test2",800,600);
+    gStyle->SetOptTitle(0);
+    gStyle->SetStatX(0.36);
+    gStyle->SetStatY(0.88);
+    gStyle->SetOptStat(0);
+    gStyle->SetLabelFont(42,"xyz");
+    gStyle->SetLabelSize(0.05,"xyz");
+    gStyle->SetLabelOffset(0.01,"xyz");
+    gStyle->SetNdivisions(510,"xyz");
+    gStyle->SetTitleFont(42,"xyz");
+    gStyle->SetTitleColor(1,"xyz");
+    gStyle->SetTitleSize(0.05,"xyz");
+    gStyle->SetTitleOffset(1.2,"xyz");
+    //gStyle->SetPalette(1);
+    
+    std::string file_name(title);
+    std::ifstream in_file;
+    in_file.open(file_name,std::ios::in);
+    
+    TGraph *g = new TGraph();
+    g->SetMarkerStyle(20);
+    g->SetMarkerColor(kYellow-4);
+    g->GetYaxis()->SetTitle("E_{truth}   [GeV]");
+    g->GetXaxis()->SetTitle("t   [X_{0}]");
+    g->GetXaxis()->CenterTitle();
+    g->GetYaxis()->CenterTitle();
+    
+    string str;
+    Int_t N = 0;
+    Double_t distance_cut = 3.5;
+    Int_t binx = 200, biny = 200;
+    
+    TH2D* h = new TH2D("Hist", "h", binx, 0, distance_cut, biny, 0, Energy);
+    h->SetMarkerStyle(7);
+    h->SetMarkerColorAlpha(kAzure+3, 1);
+    h->GetYaxis()->SetTitle("E_{truth}   [GeV]");
+    h->GetXaxis()->SetTitle("r   [cm]");
+    h->GetXaxis()->CenterTitle();
+    h->GetYaxis()->CenterTitle();
+    h->GetZaxis()->CenterTitle();
+    
+    while (std::getline(in_file, str)) {
+        std::stringstream strStream(str);
+        float dseed, distance, Eseed, energy;
+        strStream >> dseed >> distance >> Eseed >> energy;
+        if (distance > distance_cut) continue;
+        h->Fill(distance,energy);
+        N++;
+    }
+    
+    for (int i = 1; i < 201; i++){
+        for (int j = 1; j < 201; j++){
+            if (N < 30000) {if (h->GetBinContent(i,j)<2) h->SetBinContent(i,j,0);}
+            else if (h->GetBinContent(i,j)<2) h->SetBinContent(i,j,0);
+        }
+    }
+    
+    int cunt = 0;
+    int step = 9;
+    for (int i = 5; i < binx+1; i+=step){
+        Double_t mean = h->ProfileY("px",i,i+step-1)->GetMean();
+        Double_t wx = distance_cut/binx;
+        Double_t nx =( (i+(step-1)/2)*wx - wx/2  );
+        g->SetPoint(cunt, nx, mean);
+        cunt++;
+    }
+    
+    TF1* f=new TF1("f1","(FABC(x,[0],[1],[2],[3],[4]))",0,distance_cut);
+    f->SetParameters(0.8*Energy, 2.5, 0.9, 1.4, 3);
+    f->SetLineWidth(3);
+    f->SetLineColor(kRed);
+    
+    TF1* f_cp=new TF1("f_cp","exp(-1.25*x)",0,distance_cut);
+    f_cp->SetLineWidth(3);
+    f_cp->SetLineStyle(2);
+    f_cp->SetLineColor(kYellow);
+
+    c1->cd();
+    h->Draw("PCOLZ");
+    g->Draw("Psame");
+    g->Fit(f,"R");
+    f->Draw("SAME");
+    f_cp->Draw("SAME");
+    //par_file << str_Energy << " " << f->GetParameter(0) << " " << f->GetParameter(1) << " " << f->GetParameter(2) << " " << f->GetParameter(3) << " " << f->GetParameter(4) << endl;
+    
+    //TString picture_name= "doc/compare_A"+str_NO_Angle+"_FitPicture_cp/A"+str_NO_Angle+"_E"+str_Energy+"_FitPar_cp.png";
+    //c1->Print(picture_name);
+    
+    TGraph *g_Error = new TGraph();
+    g_Error->SetMarkerStyle(7);
+    g_Error->SetMarkerColorAlpha(kYellow, 0.5);
+    g_Error->GetYaxis()->SetTitle("E_{func}-E_{truth}   [GeV]");
+    g_Error->GetXaxis()->SetTitle("t   [X_{0}]");
+    g_Error->GetXaxis()->CenterTitle();
+    g_Error->GetYaxis()->CenterTitle();
+    
+    TH2D* h_Error = new TH2D("Hist_Error","h_Error",200,0,distance_cut,200,-Energy,Energy);
+    h_Error->SetMarkerStyle(7);
+    h_Error->SetMarkerColorAlpha(kAzure+3, 0.5);
+    h_Error->GetYaxis()->SetTitle("E_{func}-E_{truth}   [GeV]");
+    h_Error->GetXaxis()->SetTitle("t   [X_{0}]");
+    h_Error->GetXaxis()->CenterTitle();
+    h_Error->GetYaxis()->CenterTitle();
+    h_Error->GetZaxis()->CenterTitle();
+    
+    in_file.clear();
+    in_file.seekg(0, ios::beg);
+    N = 0;
+    while (std::getline(in_file, str)) {
+        std::stringstream strStream(str);
+        float dseed, distance, Eseed, energy;
+        strStream >> dseed >> distance >> Eseed >> energy;
+        if (distance > distance_cut) continue;
+        h_Error->Fill(distance,energy/Eseed);
+        g_Error->SetPoint(N,distance,FABC(distance,f->GetParameter(0),f->GetParameter(1),f->GetParameter(2),f->GetParameter(3),f->GetParameter(4))/FABC(dseed,f->GetParameter(0),f->GetParameter(1),f->GetParameter(2),f->GetParameter(3),f->GetParameter(4)));
+        N++;
+    }
+    c2->cd();
+    
+    h_Error->Draw("PCOLZ");
+    g_Error->Draw("Psame");
+    f->Draw("SAME");
+    f_cp->Draw("SAME");
+    
+    //TString picture_name_error= "doc/compare_A"+str_NO_Angle+"_FitPicture_cp/Error_A"+str_NO_Angle+"_E"+str_Energy+"_FitPar_cp.png";
+    //c2->Print(picture_name_error);
+    
+    in_file.close();
+    par_file.close();
+    return 0;
+}
